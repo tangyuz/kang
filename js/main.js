aea92bb4f03c1ef8c333d1d7c052bfdb3ec98f06
111 <ddd>
2016-12-02 16:25:35 +0800

    Signed-off-by: 111 <ddd>

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
   
   if(/index/.test(location.href)){
 		$("#header").load("head.html",function(){
-			
 		})
   	
   	$(".footer").load("footer.html",function(){
-  		
   	})
   	
   	
   	$(".slideBox").slide({
-    mainCell: ".bd ul",
-    autoPlay: true
-});
-jQuery(".focusBox").slide({
-    titCell: ".num li",
-    mainCell: ".pic",
-    effect: "fold",
-    autoPlay: true,
-    trigger: "click",
-    startFun: function(i) {
-        jQuery(".focusBox .txts li").eq(i).animate({
-            "bottom": 0
-        }).siblings().animate({
-            "bottom": -36
-        });
-    }
-});
+    	mainCell: ".bd ul",
+    	autoPlay: true
+		});
+		jQuery(".focusBox").slide({
+		    titCell: ".num li",
+		    mainCell: ".pic",
+		    effect: "fold",
+		    autoPlay: true,
+		    trigger: "click",
+		    startFun: function(i) {
+		        jQuery(".focusBox .txts li").eq(i).animate({
+		            "bottom": 0
+		        }).siblings().animate({
+		            "bottom": -36
+		        });
+		    }
+		});
 }
   
   if(/aboutus/.test(location.href)){

a7c345f4134afa118077c365902418271b7408de
111 <ddd>
2016-12-05 13:06:00 +0800

    Signed-off-by: 111 <ddd>

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
 
-	
-	alert(1)
-		$("#header").load("head.html",function(){
-			})
-	
-	  	$(".footer").load("footer.html",function(){
-	  	})
-	
+$("#header").load("head.html",function(){
+	})
+
+$(".footer").load("footer.html",function(){
+})
+
    
-	$(".slideBox").slide({
-    	mainCell: ".bd ul",
-    	autoPlay: true
-		});
-		$(".focusBox").slide({
-		    titCell: ".num li",
-		    mainCell: ".pic",
-		    effect: "fold",
-		    autoPlay: true,
-		    trigger: "click",
-		    startFun: function(i) {
-		        $(".focusBox .txts li").eq(i).animate({
-		            "bottom": 0
-		        }).siblings().animate({
-		            "bottom": -36
-		        });
-		    }
-		});
+$(".slideBox").slide({
+	mainCell: ".bd ul",
+	autoPlay: true
+	});
+	$(".focusBox").slide({
+	    titCell: ".num li",
+	    mainCell: ".pic",
+	    effect: "fold",
+	    autoPlay: true,
+	    trigger: "click",
+	    startFun: function(i) {
+	        $(".focusBox .txts li").eq(i).animate({
+	            "bottom": 0
+	        }).siblings().animate({
+	            "bottom": -36
+	        });
+	    }
+});
 	
 		
 
@@ -98,7 +96,7 @@
     	$(this).find(".pc").stop(true).slideUp();
     })
   	
-jQuery(".ruwangyiyuan").slide({
+$(".ruwangyiyuan").slide({
     titCell: ".hdm ul",
     mainCell: ".bdm1 ul",
     autoPage: true,
@@ -119,7 +117,7 @@ $(function() {
 });
   	
   	
-  }
+}
   
   if(/sma/.test(location.href)){
   	$("#header").load("head.html",function(){
@@ -129,6 +127,47 @@ $(function() {
   	$(".footer").load("footer.html",function(){
   		
   	})
+  }
+  
+  if(/news/.test(location.href)){
+  	$.ajax(
+				{
+					type:"get",
+					url:"data/data.json",
+					async:true,
+					datatype:"json",
+					success:function(mag){
+						var data=mag.data;
+						var str='';
+						for(var i in data){
+							str+='<li><a href="javascript:;" dataid="'+data[i].id+'">'+data[i].name+'</a><span>'+data[i].time+'</span></li>'
+							
+							
+							
+//							str+='<div class="l yi">'
+//							str+='<a dataid="'+data[i].id+'" class="atu" href="javascript:;"><img  src="'+data[i].img+'"/></a>'
+//							str+='<a class="a2" dataid="'+data[i].id+'"  href="javascript:;">'+data[i].name+'</a>'
+//							str+='<div class="di">'
+//							str+='<p class="l p1">'+data[i].name1+'</p>'
+//							str+='<p class="l p2">'+data[i].name2+'</p>'
+//							str+='</div>'
+//							str+='<div class="shu">'
+//							str+=data[i].id		
+//							str+='</div>'	
+//							str+='<a dataid="'+data[i].id+'" class="a3" href="javascript:;"><img src="'+data[0].img1+'"/></a>'
+//							str+='</div>'	
+							
+						}
+						$(".ul1").append(str);
+						
+						
+						
+						
+					}
+					
+					
+				}
+	)
   }
 
 
